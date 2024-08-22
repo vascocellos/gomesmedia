1cd8f00a4a46206648f3e428e1da633d35c83b29
vascocellos <vasco.cellos@tutamail.com>
2024-08-22 07:21:04 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ... (Animação de rolagem da resposta anterior) ...
+
+// Adicione mais interações com JavaScript, como:
+// - Abrir/fechar menus responsivos
+// - Carregar conteúdo dinamicamente com AJAX
+// - Criar animações e efeitos visuais
+// - Validar formulários
